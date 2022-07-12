033bf66baa8a6846c6e498c03c6fe5659158b952
Agustin Schulze <agsp3rs3o@gmail.com>
2022-07-11 22:03:20 -0300

    Añadiendo Ejercicio 3 y actualizando Ejercicios de Herencia

---
+++ Java Excepciones/Ejercicio3/src/main/java/com/mycompany/ejercicio3/Ejercicio3.java
@@ -0,0 +1,47 @@
+package com.mycompany.ejercicio3;
+
+import Entidades.DivisionNumero;
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+    private static final Scanner leer = new Scanner(System.in).useDelimiter("\n");
+
+    public static void main(String[] args) {
+
+        DivisionNumero divN = new DivisionNumero();
+
+        try {
+
+            System.out.println("Ingres el primer numero");
+
+            divN.setN1(leer.next());
+
+            System.out.println("Ingrese el segundo numero");
+
+            divN.setN2(leer.next());
+
+            int n1Int = Integer.parseInt(divN.getN1());
+
+            int n2Int = Integer.parseInt(divN.getN2());
+
+            int division = n1Int / n2Int;
+
+            System.out.println("La división es: " + division);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Excepción hija");
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error: " + e.toString());
+
+        } catch (Exception e) {
+
+            System.out.println("Excepción madre");
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error: " + e.toString());
+
+        }
+
+    }
+}
