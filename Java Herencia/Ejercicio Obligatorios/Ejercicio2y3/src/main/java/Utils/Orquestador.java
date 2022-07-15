27191f5c832fa3674a177f400c4e819be81b8a99
Agustin <agsp3rs3o@gmail.com>
2022-07-15 12:07:20 -0300

    Arreglando metodo precioFinal() en orquestador

--- Java Herencia/Ejercicio Obligatorios/Ejercicio2y3/src/main/java/Utils/Orquestador.java
+++ Java Herencia/Ejercicio Obligatorios/Ejercicio2y3/src/main/java/Utils/Orquestador.java
@@ -7,40 +7,33 @@ import java.util.Scanner;
 
 public class Orquestador {
 
-    public static final ArrayList<Electrodomestico> listaElectrodomesticos = new ArrayList<>();
     private static final Scanner leer = new Scanner(System.in).useDelimiter("\n");
 
     public static void metodoOrquestador() {
 
-        int precioTeles = 0;
-        int precioLavadoras = 0;
-        int precioTotal;
+        ArrayList<Electrodomestico> listaElectrodomesticos = new ArrayList<>();
 
         System.out.println(Constantes.CANTIDAD_LAVADORAS);
 
         int cantLavadoras = leer.nextInt();
 
-        crearLavadora(cantLavadoras);
+        crearLavadora(cantLavadoras, listaElectrodomesticos);
 
         System.out.println(Constantes.CANTIDAD_TELES);
 
         int cantidadTelevisores = leer.nextInt();
 
-        crearTelevisor(cantidadTelevisores);
+        crearTelevisor(cantidadTelevisores, listaElectrodomesticos);
 
-        calcularPrecioFinal(precioLavadoras, precioTeles);
-
-        precioTotal = precioTeles + precioLavadoras;
-
-        System.out.println(Constantes.PRECIO_LAVADORAS + precioLavadoras);
-
-        System.out.println(Constantes.PRECIO_TELES + precioTeles);
-
-        System.out.println(Constantes.PRECIO_TOTAL + precioTotal);
+        calcularPrecioFinal(listaElectrodomesticos);
 
     }
 
-    public static void calcularPrecioFinal(int precioLavadoras, int precioTeles) {
+    public static void calcularPrecioFinal(ArrayList<Electrodomestico> listaElectrodomesticos) {
+
+        int precioTeles = 0;
+        int precioLavadoras = 0;
+        int precioTotal;
 
         for (Electrodomestico aux : listaElectrodomesticos) {
 
@@ -58,9 +51,17 @@ public class Orquestador {
 
         }
 
+        precioTotal = precioTeles + precioLavadoras;
+
+        System.out.println(Constantes.PRECIO_LAVADORAS + precioLavadoras);
+
+        System.out.println(Constantes.PRECIO_TELES + precioTeles);
+
+        System.out.println(Constantes.PRECIO_TOTAL + precioTotal);
+
     }
 
-    public static void crearTelevisor(int cantidadTelevisores) {
+    public static void crearTelevisor(int cantidadTelevisores, ArrayList<Electrodomestico> listaElectrodomesticos) {
 
         for (int i = 0; i < cantidadTelevisores; i++) {
 
@@ -73,7 +74,7 @@ public class Orquestador {
         }
     }
 
-    public static void crearLavadora(int cantLavadoras) {
+    public static void crearLavadora(int cantLavadoras, ArrayList<Electrodomestico> listaElectrodomesticos) {
 
         for (int i = 0; i < cantLavadoras; i++) {
 
