57ba4a921e8b5cdc5f93f4d84fbabe187e3fb905
Agustin <agsp3rs3o@gmail.com>
2022-09-01 21:35:30 -0300

    Añadiendo metodos al orquestador

--- Java JPA/Ejercicios Obligatorios/Ejercicio1/src/Libreria/Utils/Orquestador.java
+++ Java JPA/Ejercicios Obligatorios/Ejercicio1/src/Libreria/Utils/Orquestador.java
@@ -2,9 +2,13 @@ package Libreria.Utils;
 
 import java.util.Scanner;
 import Libreria.Constantes.Constantes;
+import Libreria.Entidades.Autor;
+import Libreria.Entidades.Editorial;
 import Libreria.Servicios.AutorServicios;
+import Libreria.Servicios.ClienteServicios;
 import Libreria.Servicios.EditorialServicios;
 import Libreria.Servicios.LibroServicios;
+import Libreria.Servicios.PrestamoServicios;
 
 public class Orquestador {
 
@@ -12,6 +16,8 @@ public class Orquestador {
     private static final LibroServicios libroServicios = new LibroServicios();
     private static final AutorServicios autorServicios = new AutorServicios();
     private static final EditorialServicios editorialServicios = new EditorialServicios();
+    private static final ClienteServicios clienteServicios = new ClienteServicios();
+    private static final PrestamoServicios prestamosServicios = new PrestamoServicios();
 
     public static void menuprincipal() throws Exception {
 
@@ -35,11 +41,17 @@ public class Orquestador {
                 case 3:
                     menuEditorial();
                     break;
+                case 4:
+                    menuCliente();
+                    break;
+                case 5:
+                    menuPrestamo();
+                    break;
                 default:
                     System.out.println(Constantes.OPCION_INCORRECTA);
             }
 
-        } while (opc != 4);
+        } while (opc != 6);
         System.out.println(Constantes.SALIDA);
 
     }
@@ -59,7 +71,8 @@ public class Orquestador {
             switch (opc) {
                 case 1:
                     System.out.println(Constantes.INGRESE_NOMBRE_AUTOR);
-                    autorServicios.guardarAutor();
+                    String nombre = leer.next();
+                    autorServicios.crearAutor(nombre, autorServicios.pedirAltaAutor());
                     break;
                 case 2:
                     autorServicios.mostrarTodosLosAutores().forEach(System.out::println);
@@ -109,7 +122,19 @@ public class Orquestador {
 
             switch (opc) {
                 case 1:
-                    libroServicios.guardarLibro();
+                    System.out.println(Constantes.INGRESE_ISBN);
+                    Long isbn = leer.nextLong();
+                    System.out.println(Constantes.INGRESE_TITULO);
+                    String titulo = leer.next();
+                    System.out.println(Constantes.INGRESE_ANIO_LANZAMIENTO);
+                    Integer anio = leer.nextInt();
+                    System.out.println(Constantes.INGRESE_COPIAS_LIBROS);
+                    Integer copias = leer.nextInt();
+                    System.out.println(Constantes.INGRESE_NOMBRE_AUTOR);
+                    Autor autores = libroServicios.pedirAutor(leer.next());
+                    System.out.println(Constantes.INGRESE_NOMBRE_EDITORIAL);
+                    Editorial editoriales = libroServicios.pedirEditorial(leer.next());
+                    libroServicios.crearLibro(isbn, titulo, anio, copias, autores, editoriales);
                     break;
                 case 2:
                     libroServicios.mostrarTodosLosLibros().forEach(System.out::println);
@@ -164,7 +189,8 @@ public class Orquestador {
             switch (opc) {
                 case 1:
                     System.out.println(Constantes.INGRESE_NOMBRE_EDITORIAL);
-                    editorialServicios.guardarEditorial();
+                    String nombre = leer.next();
+                    editorialServicios.crearEditorial(nombre, editorialServicios.pedirAltaEditorial());
                     break;
                 case 2:
                     editorialServicios.mostrarTodasEditoriales().forEach(System.out::println);
@@ -197,6 +223,68 @@ public class Orquestador {
             }
 
         } while (opc != 8);
+
+    }
+
+    public static void menuCliente() throws Exception {
+
+        int opc;
+
+        do {
+            System.out.println(Constantes.OPCIONES_MENU_CLIENTE);
+
+            System.out.println(Constantes.INGRESE_OPCION);
+
+            opc = leer.nextInt();
+
+            switch (opc) {
+                case 1:
+                    System.out.println(Constantes.INGRESE_DOCUMENTO_CLIENTE);
+                    Long dni = leer.nextLong();
+                    System.out.println(Constantes.INGRESE_NOMBRE_CLIENTE);
+                    String nombre = leer.next();
+                    System.out.println(Constantes.INGRESE_APELLIDO_CLIENTE);
+                    String apellido = leer.next();
+                    System.out.println(Constantes.INGRESE_TELEFONO_CLIENTE);
+                    String telefono = leer.next();
+                    System.out.println(clienteServicios.crearCliente(dni, nombre, apellido, telefono));
+                    break;
+                case 2:
+                    clienteServicios.mostrarTodosLosClientes().forEach(System.out::println);
+                    break;
+                case 3:
+                    System.out.println(Constantes.INGRESE_NOMBRE_CLIENTE);
+                    clienteServicios.mostrarClientePorNombre(leer.next()).forEach(System.out::println);
+                    break;
+                case 4:
+                    System.out.println(Constantes.INGRESE_ID_CLIENTE);
+                    System.out.println(clienteServicios.mostrarClientePorID(leer.next()));
+                    break;
+                case 5:
+                    System.out.println(Constantes.INGRESE_ID_CLIENTE);
+                    String id = leer.next();
+                    System.out.println(Constantes.INGRESE_DOCUMENTO_CLIENTE);
+                    dni = leer.nextLong();
+                    System.out.println(Constantes.INGRESE_NOMBRE_CLIENTE);
+                    nombre = leer.next();
+                    System.out.println(Constantes.INGRESE_APELLIDO_CLIENTE);
+                    apellido = leer.next();
+                    System.out.println(Constantes.INGRESE_TELEFONO_CLIENTE);
+                    telefono = leer.next();
+                    System.out.println(clienteServicios.modificarClientePorID(id, nombre, apellido, dni, telefono));
+                    break;
+                case 6:
+                    menuprincipal();
+                    break;
+                default:
+                    System.out.println(Constantes.OPCION_INCORRECTA);
+            }
+
+        } while (opc != 6);
+
+    }
+
+    public static void menuPrestamo() throws Exception {
 
     }
 
